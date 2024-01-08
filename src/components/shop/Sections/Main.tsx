bbfddc339cf7fadbe165da913ec616ce5c0f3b28
Bartosz Ślusarczyk <github.bartek@gmail.com>
2024-01-08 20:19:38 +0100

    added forgotten ids

--- src/components/shop/Sections/Main.tsx
+++ src/components/shop/Sections/Main.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import './Sections.scss';
 import ShopTile from '../../UI/ShopTile';
 
 export type tileProps = {
+	id: string;
 	background: string;
 	content: any;
 	minWidth: string;
@@ -13,6 +15,7 @@ export type tileProps = {
 
 const tiles: tileProps[] = [
 	{
+		id: 't1',
 		background: `linear-gradient(31deg, #E6E5FF -10.43%, #635EF7 110.03%)`,
 		content: (
 			<div className='reviews'>
@@ -36,6 +39,7 @@ const tiles: tileProps[] = [
 		small: true,
 	},
 	{
+		id: 't2',
 		background: `#E6E5FF`,
 		content: (
 			<div className='shopping-ad'>
@@ -55,6 +59,7 @@ const tiles: tileProps[] = [
 		small: false,
 	},
 	{
+		id: 't3',
 		background: `#F0F0F0`,
 		content: (
 			<div className='active-products'>
@@ -94,6 +99,7 @@ const tiles: tileProps[] = [
 		small: false,
 	},
 	{
+		id: 't4',
 		background: `url('https://img.freepik.com/free-photo/shopping-cart-with-gift-box-icon-promotion-discount-sale-reward-checkout-ecommerce-online-shopping-3d-illustration_56104-2102.jpg?w=1380&t=st=1704475806~exp=1704476406~hmac=75009b2bce06378e0bd5c9e15156b19ba42be28c99e11d0dc805b183f8f8f717') center/cover`,
 		content: (
 			<div className='more-info'>
@@ -106,6 +112,7 @@ const tiles: tileProps[] = [
 		small: false,
 	},
 	{
+		id: 't5',
 		background: `linear-gradient(335deg, #2A61DB 0%, #D9E4FC 210.78%)`,
 		content: (
 			<div className='policyTerms-link'>
@@ -130,22 +137,79 @@ const tiles: tileProps[] = [
 ];
 
 const MainSection: React.FC = () => {
+	const transitionSett = { duration: 0.5 };
+
 	return (
 		<section className='section section--main'>
-			<h2 className='section--main__header'>
+			<motion.h2
+				className='section--main__header'
+				variants={{
+					hidden: { opacity: 0.5 },
+					visible: {
+						opacity: 1,
+					},
+				}}
+				exit={{ opacity: 1 }}
+				transition={transitionSett}
+				initial='hidden'
+				animate='visible'>
 				A place where you can
-				<br />
-				find everything
-				<br />
-				whatever <span>you desire</span>
-			</h2>
-			<p className='section--main__miniText'>
+				<motion.span
+					className='change-place'
+					variants={{
+						hidden: { opacity: 0, y: 40 },
+						visible: { opacity: 1, y: 0 },
+					}}
+					exit={{ opacity: 1, y: 0 }}
+					initial='hidden'
+					animate='visible'
+					transition={transitionSett}>
+					find everything
+					<br />
+					whatever <span className='gradient'>you desire</span>
+				</motion.span>
+			</motion.h2>
+			<motion.p
+				className='section--main__miniText'
+				variants={{
+					hidden: { opacity: 0, scale: 0 },
+					visible: {
+						opacity: 1,
+						scale: [0.7, 1.2, 1],
+					},
+				}}
+				initial='hidden'
+				animate='visible'
+				transition={transitionSett}>
 				Thousands of products at your fingertips
-			</p>
+			</motion.p>
 			<div className='section--main__links'>
-				<Link to='' className='newsletter'>
-					<p>Join to us for FREE</p>
-					<i className='fa-solid fa-share'></i>
+				<Link to=''>
+					<motion.div
+						className='newsletter'
+						variants={{
+							hidden: { padding: 0 },
+							visible: {
+								padding: '0.7rem 2.5rem',
+							},
+						}}
+						initial='hidden'
+						animate='visible'
+						transition={transitionSett}
+						exit={{ padding: '0.7rem 2.5rem' }}>
+						<motion.div
+							className='newsletter--content'
+							variants={{
+								hidden: { opacity: 0, y: 20 },
+								visible: { opacity: 1, y: 0 },
+							}}
+							initial='hidden'
+							animate='visible'
+							transition={transitionSett}>
+							<p>Join to us for FREE</p>
+							<i className='fa-solid fa-share'></i>
+						</motion.div>
+					</motion.div>
 				</Link>
 				<Link to='../products' className='allProducts'>
 					One Click All Products
@@ -153,7 +217,7 @@ const MainSection: React.FC = () => {
 			</div>
 			<div className='section--main__dashboard'>
 				{tiles.map((tile) => (
-					<ShopTile tile={tile} />
+					<ShopTile key={tile.id} tile={tile} />
 				))}
 			</div>
 		</section>
