0fbbdec9b14329e1016263f478dc71275eb384f8
Bartosz Ślusarczyk <github.bartek@gmail.com>
2024-01-11 20:30:58 +0100

    Added animations to Products section

--- src/components/shop/Sections/PopProducts.tsx
+++ src/components/shop/Sections/PopProducts.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import './Sections.scss';
 import StarTile from '../../UI/StarTile';
+import { useEffect, useMemo, useRef } from 'react';
+import { motion, useAnimation, AnimationControls } from 'framer-motion';
 
 export type hashtagsType = {
 	id: string;
@@ -71,30 +73,107 @@ const DUMMY_STARPRODUCTS: Starproducts[] = [
 	},
 ];
 
+export type animationType = {
+	animate: AnimationControls;
+	transition: {
+		duration: number;
+		type?: string;
+	};
+	initial: {
+		scale?: number;
+		opacity?: number;
+		y?: number;
+	};
+};
+
 const PopProducts: React.FC = () => {
+	const sectionRef = useRef<HTMLDivElement>(null);
+	const scaleAnimationF = useAnimation();
+	const showUpAnimationF = useAnimation();
+	const controls: AnimationControls = useAnimation();
+	const brandNameAnimation: AnimationControls = useAnimation();
+
+	const scaleAnimation: animationType = useMemo(() => {
+		return {
+			animate: scaleAnimationF,
+			transition: { duration: 0.75, type: 'string' },
+			initial: {
+				scale: 1,
+			},
+		};
+	}, [scaleAnimationF]);
+
+	const showUp: animationType = useMemo(() => {
+		return {
+			animate: showUpAnimationF,
+			initial: { opacity: 0, y: -30 },
+			transition: { duration: 1 },
+		};
+	}, [showUpAnimationF]);
+
+	useEffect(() => {
+		let isAnimating = false;
+
+		const handleScroll = () => {
+			const element = sectionRef.current;
+			if (element) {
+				const rect = element.getBoundingClientRect();
+				const isVisible =
+					rect.top + 200 < window.innerHeight && rect.bottom >= 0;
+				if (isVisible && !isAnimating) {
+					isAnimating = true;
+
+					element.scrollIntoView({
+						block: 'start',
+					});
+					controls.start({ opacity: 1 });
+					showUp.animate.start({ opacity: 1, y: 0 });
+					scaleAnimation.animate.start({ scale: [0.95, 1.05, 0.95, 1] });
+					brandNameAnimation.start({ opacity: 1, x: 0, y: 0 });
+				}
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, [brandNameAnimation, controls, scaleAnimation, showUp.animate]);
+
 	return (
-		<section className='section section--popProducts'>
+		<motion.div
+			ref={sectionRef}
+			initial={{ opacity: 0 }}
+			animate={controls}
+			className='section section--popProducts'>
 			<div className='first-row'>
-				<header className='section--popProducts__header'>
+				<motion.header {...showUp} className='section--popProducts__header'>
 					MOST
 					<br /> POPULAR
-				</header>
-				<p className='section--popProducts__intro'>
+				</motion.header>
+				<motion.p {...showUp} className='section--popProducts__intro'>
 					Today's list of most popular products is guided by the best reviewed
 					and most purchased products
-				</p>
-				<h3 className='brand--name'>Xavy</h3>
+				</motion.p>
+				<motion.h3
+					initial={{ opacity: 0, x: -600, y: -200 }}
+					animate={brandNameAnimation}
+					transition={{ duration: 1 }}
+					className='brand--name'>
+					Xavy
+				</motion.h3>
 			</div>
 			<div className='section--popProducts__content'>
 				<div className='left-side'>
-					<div className='big-card card-SHT'>
+					<motion.div {...scaleAnimation} className='big-card card-SHT'>
 						<div className='svg'></div>
 						<div className='content'>
 							<p>New SHT collections</p>
 							<Link to=''>Check now</Link>
 						</div>
-					</div>
-					<div className='big-card card-SPTS'>
+					</motion.div>
+					<motion.div {...scaleAnimation} className='big-card card-SPTS'>
 						<div className='content'>
 							<p>Sport assortment from SPTS</p>
 						</div>
@@ -102,32 +181,38 @@ const PopProducts: React.FC = () => {
 						<Link to='' className='product-link'>
 							<i className='fa-solid fa-arrow-right'></i>
 						</Link>
-					</div>
+					</motion.div>
 					<div className='parse-cards'>
-						<div className='mini-card card-trending'>
+						<motion.div {...scaleAnimation} className='mini-card card-trending'>
 							<header>Trending hashtags</header>
 							<div className='content'>
 								{trendingHashtags.map((item) => (
-									<div className='hashtag-block' key={item.id}>
+									<div key={item.id} className='hashtag-block'>
 										{item.text}
 									</div>
 								))}
 							</div>
-						</div>
-						<div className='mini-card card-promoCode'>
+						</motion.div>
+						<motion.div
+							{...scaleAnimation}
+							className='mini-card card-promoCode'>
 							<header>New Promo Code</header>
 							<p className='code-percent'>-19%</p>
 							<span>Join newsletter only</span>
-						</div>
+						</motion.div>
 					</div>
 				</div>
 				<div className='right-side'>
 					<div className='parse-cards'>
 						{DUMMY_STARPRODUCTS.map((product) => (
-							<StarTile key={product.id} product={product} />
+							<StarTile
+								key={product.id}
+								product={product}
+								animation={scaleAnimation}
+							/>
 						))}
 					</div>
-					<div className='bottom-card'>
+					<motion.div {...scaleAnimation} className='bottom-card'>
 						<div className='content'>
 							<header>New furniture!</header>
 							<p>Check out category</p>
@@ -139,10 +224,10 @@ const PopProducts: React.FC = () => {
 						<Link to='' className='product-link'>
 							<i className='fa-solid fa-arrow-right'></i>
 						</Link>
-					</div>
+					</motion.div>
 				</div>
 			</div>
-		</section>
+		</motion.div>
 	);
 };
 export default PopProducts;
