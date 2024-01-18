9b3d90159682f1fa2cd0e4a9adcb401755619984
Bartosz Ślusarczyk <github.bartek@gmail.com>
2024-01-18 17:52:11 +0100

    created cart Slice in Redux store

---
+++ src/store/cart-redux.ts
@@ -0,0 +1,60 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export type productParams = {
+	id: string;
+	title: string;
+	color: string;
+	size: string;
+	quantity: number;
+	shippingType: string;
+	price: number;
+	image: string;
+};
+
+const initialState: { items: productParams[]; totalQuantity: number } = {
+	items: [],
+	totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+	name: 'cart',
+	initialState,
+	reducers: {
+		addToCart(state, action) {
+			const newItem: productParams = action.payload;
+			const itemId = newItem.id += newItem.color.charAt(0) + newItem.size.charAt(0)
+			const exsistingItem = state.items.find((item) => item.id === itemId);
+			state.totalQuantity++;
+			if (!exsistingItem) {
+				state.items.push(newItem);
+			} else {
+				if (
+					exsistingItem.color !== newItem.color ||
+					exsistingItem.size !== newItem.size
+				) {
+					state.items.push(newItem);
+				} else {
+					exsistingItem.quantity += newItem.quantity;
+					exsistingItem.price += newItem.price;
+					if (exsistingItem.shippingType !== newItem.shippingType) {
+						exsistingItem.shippingType = newItem.shippingType;
+					}
+				}
+			}
+		},
+		removeFromCart(state, action) {
+			const id = action.payload;
+			const exsistingItem = state.items.find((item) => item.id === id);
+			state.totalQuantity--;
+			if (exsistingItem!.quantity === 1) {
+				state.items = state.items.filter((item) => item.id !== id);
+			} else {
+				exsistingItem!.quantity--;
+			}
+		},
+	},
+});
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
