bafa66aba68e761c274412206f38a985d7ad26ac
Szubzdov <93838137+Szubzdov@users.noreply.github.com>
2024-01-23 17:58:42 +0100

    Add files via upload

---
+++ backend/models/Auth.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const Auth = new mongoose.Schema({
+    _id: String,
+    firstName: String,
+    surName: String,
+    email: String,
+    password: String,
+    phoneNum:String, 
+    image: String, 
+    money: Number,
+    cart_id: String
+})
+
+module.exports= mongoose.model('auths',Auth)
