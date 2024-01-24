c43755a71395f64c6a0956ae974750dcb01f8f99
Bartosz Ślusarczyk <github.bartek@gmail.com>
2024-01-24 19:58:29 +0100

    added userid as remove from localStorage while logout

--- frontend/src/pages/Logout.tsx
+++ frontend/src/pages/Logout.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'react-router-dom';
 export const action = () => {
 	localStorage.removeItem('token');
 	localStorage.removeItem('expiration');
+	localStorage.removeItem('userid')
 
 	return redirect('/auth');
 };
